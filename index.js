60948077151cae1cd86f031168f7bb6fba679e2e
Fahad <fahad@Fahads-MacBook-Air.local>
2018-11-26 15:59:19 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (requestAnimationFrame, res) => {
+    res.send({hi: 'there'});
+
+});
+
+
+const POST = process.env.PORT || 5000;
+app.listen(5000);
