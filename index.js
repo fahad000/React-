b5f998c5b3c0b145317db3cce71c2dafac6bc279
Fahad <fahad@Fahads-MacBook-Air.local>
2018-11-27 11:41:05 -0500

    Passport

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('./config/Keys');
+
+
+
 const app = express();
+// app.get('/', (requestAnimationFrame, res) => {
+//     res.send({hi: 'there'});
 
-app.get('/', (requestAnimationFrame, res) => {
-    res.send({hi: 'there'});
+// });
 
-});
+passport.use(new GoogleStrategy({
+    clientID: Keys.googleClientID,
+    clientSecret: Keys.googleClientSecret,
+    callbackURL: 'auth/google/callback'
+},
+accessToken => {
+    console.log(accessToken);
+}
+));
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
