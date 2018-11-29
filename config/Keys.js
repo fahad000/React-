7d1ca91d066ef6ea8b1c96d913ec213b7513b95b
Fahad <fahad@Fahads-MacBook-Air.local>
2018-11-29 11:51:48 -0500

    production

--- config/Keys.js
+++ config/Keys.js
@@ -1,6 +1,6 @@
 //keys.js figure out what set of credentials to return 
 
-if (process.env.NODE_ENV === 'PRODUCTION'){
+if (process.env.NODE_ENV === 'production'){
 
     // we are in production - return the prod set of keys
 
